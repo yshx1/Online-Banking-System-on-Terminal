e969357fc65f676e13cfca13b9444eaaf6ae9ead
yshx1 <159809857+yshx1@users.noreply.github.com>
2024-07-14 17:33:38 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,38 @@
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid amount!");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        } else {
+            System.out.println("Invalid amount or insufficient balance!");
+        }
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return accountNumber + "," + accountHolderName + "," + balance;
+    }
+}
